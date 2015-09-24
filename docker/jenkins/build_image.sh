394bd2ffdd8ad289c9aeb8adc0caf993e0287db1
Giorgos Logiotatidis <seadog@sealabs.net>
2015-09-24 18:40:50 +0300

    Build if image does not exist.

--- docker/jenkins/build_image.sh
+++ docker/jenkins/build_image.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 #
 set -ex
+DOCKER_IMAGE_TAG=${DOCKER_REPOSITORY}:${GIT_COMMIT}
+
+# If docker image exists and no force rebuild do nothing
+FORCE_REBUILD=`echo "$FORCE_REBUILD" | tr '[:upper:]' '[:lower:]'`
+if [[ $FORCE_REBUILD != "true" ]];
+then
+    if docker history -q $DOCKER_IMAGE_TAG 2> /dev/null;
+    then
+        echo "Docker image already exists, do nothing"
+        exit 0;
+    fi
+fi
+
 
 # TODO comment
 find . -newerat 20140101 -exec touch -t 201401010000 {} \;
 
 cat docker/dockerfiles/${DOCKERFILE} | envsubst > Dockerfile
-DOCKER_IMAGE_TAG=${DOCKER_REPOSITORY}:${GIT_COMMIT}
 docker build -t $DOCKER_IMAGE_TAG .
 
 # TODO only run docker-squash if image is new
